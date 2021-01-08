bcbef3c4658f4113d84dafeb1cee565b25dde7b6
윤수현 <96-05-03@naver.com>
2021-01-08 18:10:59 +0900

    mydev

--- src/main/java/com/example/shop/shop/HelloController.java
+++ src/main/java/com/example/shop/shop/HelloController.java
@@ -6,8 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("/hello")
+	@GetMapping("/hello1")
 	public String hello(){
 		return "hello";
 	}
+	@GetMapping("bye")
+	public String bye(){
+		return "bye";
+	}
+	
 }
