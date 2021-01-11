0f01439e6880b8a35383e706469ed3adc280fa5d
윤수현 <96-05-03@naver.com>
2021-01-11 09:46:19 +0900

    YoonSuHyeon2 rebase

--- src/main/java/com/example/shop/shop/HelloController.java
+++ src/main/java/com/example/shop/shop/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@GetMapping("YoonSuHyeon1"")
+	@GetMapping("YoonSuHyeon2")
 
 	public String hello() {
 		return "hello";
