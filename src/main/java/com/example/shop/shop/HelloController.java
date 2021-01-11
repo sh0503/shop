7403ca49a0e65a66b51f73a53cfa8e39fd8e4c4e
윤수현 <96-05-03@naver.com>
2021-01-11 09:31:01 +0900

    commit

--- src/main/java/com/example/shop/shop/HelloController.java
+++ src/main/java/com/example/shop/shop/HelloController.java
@@ -1,38 +1,29 @@
 package com.example.shop.shop;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-
-	@GetMapping("/hello2")
-	public String hello(){
+	@GetMapping("/h22ello223")
+	public String hello() {
 		return "hello";
 	}
 
-<<<<<<< HEAD
 	@GetMapping("bye")
-=======
-	@GetMapping("SuHyeonbye2")
-
->>>>>>> test
-	public String bye(){
+	public String bye() {
 		return "bye";
 	}
 
 	@GetMapping("nice2")
-	public String nice(){
+	public String nice() {
 		return "nice";
 	}
+
 	@GetMapping("see")
-	public String see(){
+	public String see() {
 		return "see";
 	}
-
-	
-
 
 }
